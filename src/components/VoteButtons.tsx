e103f08155e153446162982d3a009ab437ce5bd8
banp95 <alexisdev95@gmail.com>
2024-09-02 11:04:37 -0500

    fix:create componente vote

---
+++ src/components/VoteButtons.tsx
@@ -0,0 +1,25 @@
+import { FaMinus, FaPlus } from "react-icons/fa";
+
+export const VoteButtons: React.FC<{
+  userVote: number | null;
+  score: number;
+  onVote: (increment: number) => void;
+}> = ({ userVote, score, onVote }) => (
+  <div className="flex gap-4 flex-row lg:flex-col bg-veryLightGray p-4 rounded-lg ">
+    <button
+      className={userVote === 1 ? "cursor-not-allowed" : "cursor-pointer"}
+      onClick={() => onVote(1)}
+      disabled={userVote === 1}
+    >
+      <FaPlus className="text-lightGrayishBlue group-hover:text-moderateBlue" />
+    </button>
+    <span className="text-moderateBlue font-bold">{score}</span>
+    <button
+      className={userVote === null ? "cursor-not-allowed" : "cursor-pointer"}
+      onClick={() => onVote(-1)}
+      disabled={userVote === null}
+    >
+      <FaMinus className="text-lightGrayishBlue group-hover:text-moderateBlue" />
+    </button>
+  </div>
+);
